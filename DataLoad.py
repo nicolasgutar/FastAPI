25d55599bfe71bd337ca0d2899dd2e51a751b2e9
nico <gutierrez23nicolas@gmail.com>
2024-09-26 07:19:16 -0500

    requirements.txt creado

---
+++ DataLoad.py
@@ -0,0 +1,13 @@
+#Script para cargar csv a SQL desde python
+
+connection = psycopg2.connect(host = 'localhost',
+                              database = 'northwind_olap',
+                              user='user',
+                              password = 'password',
+                              port=5433)
+
+cursor = connection.cursor()
+cursor.execute("DROP TABLE IF EXISTS CropData")
+
+
+
--- router.py
+++ router.py
@@ -13,12 +13,12 @@ def get_db():
     finally:
         db.close()
 
-@router.post('/create')
+@router.post('/')
 async def create_crop_data(crop_data: RequestCropData, db: Session = Depends(get_db)):
     created_data = methods.create_crop_data(db, crop_data.parameter)
     return Response(code='200', status='Ok', message="Crop_data created successfully", result=created_data).dict(exclude_none=True)
 
 @router.get("/")
 async def get_data(db: Session = Depends(get_db)):
-    _data = methods.get_data(db,0,100)
+    _data = methods.get_data(db,100,100)
     return Response(code = '200', status='Ok', message="Crop_data fetched successfully", result=_data).dict(exclude_none=True)
