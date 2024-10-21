a5014029d87664b4f2ce075f77bbf3dcf676b8f3
nico <gutierrez23nicolas@gmail.com>
2024-10-21 07:59:02 -0500

    desde s3

--- methods.py
+++ methods.py
@@ -1,13 +1,15 @@
 from sqlalchemy.orm import Session
 from model import CropData
 from schemas import CropDataSchema
-
+import boto3
+from botocore.exceptions import NoCredentialsError
+import json
 
 #get all data
 def get_data(db: Session, skip: int = 0, limit: int = 100):
     return db.query(CropData).offset(skip).limit(limit).all()
 
-#create an entry
+#create an entry in the database
 def create_crop_data(db: Session, crop_data: CropDataSchema):
     db_crop_data = CropData(state_name=crop_data.state_name, district_name=crop_data.district_name,
                             crop_year=crop_data.crop_year, season=crop_data.season, crop=crop_data.crop,
@@ -23,3 +25,11 @@ def remove_crop_data(db: Session, id: int):
     db.delete(crop_data)
     db.commit()
     return crop_data
+
+def save_to_s3(data: dict, bucket_name: str, object_name: str):
+    s3 = boto3.client('s3')
+    try:
+        s3.put_object(Bucket=bucket_name, Key=object_name, Body=json.dumps(data))
+        return {"status": "success", "message": "Data saved to S3 successfully"}
+    except NoCredentialsError:
+        return {"status": "error", "message": "Credentials not available"}
--- router.py
+++ router.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter, Depends, Path, Query
+from fastapi import APIRouter, HTTPException, Depends, Path, Query
 from config import SessionLocal
 from sqlalchemy.orm import Session
-from schemas import RequestCropData, Response
+from schemas import CropDataSchema, RequestCropData, Response
 import methods
 
 router = APIRouter()
-
-
 
 def get_db():
     db = SessionLocal()
@@ -15,10 +13,19 @@ def get_db():
     finally:
         db.close()
 
-@router.post('/')
-async def create_crop_data(crop_data: RequestCropData, db: Session = Depends(get_db)):
+@router.post('/db')
+async def create_crop_data_db(crop_data: RequestCropData, db: Session = Depends(get_db)):
     created_data = methods.create_crop_data(db, crop_data.parameter)
     return Response(code='200', status='Ok', message="Crop_data created successfully", result=created_data).dict(exclude_none=True)
+
+@router.post('/s3')
+async def create_crop_data_s3(crop_data: RequestCropData):
+    data = crop_data.dict()
+    result = methods.save_to_s3(data, bucket_name='user-08-smm-ueia-so', object_name='crop_data.json')
+    if result['status'] == 'success':
+        return Response(code='200', status='Ok', message=result['message'], result=data).dict(exclude_none=True)
+    else:
+        raise HTTPException(status_code=500, detail=result['message'])
 
 @router.get("/")
 async def get_data(skip: int = Query(0), limit: int = Query(100), db: Session = Depends(get_db)):
